66ff354b01e45c2c4822aeaaf77ed48c0da84be2
White <mwhite@webassign.net>
2017-03-14 12:53:44 -0400

    Files

---
+++ gameModules/bgFull.gameBoard.js
@@ -0,0 +1,10 @@
+(function () {
+      angular.module('bFull.gameBoard')
+        .controller('gameBoardCtrl', gameBoardController);
+
+    gameBoardController.$inject = [];
+
+    function gameBoardController() {
+        const vm = this;
+    }
+}());
