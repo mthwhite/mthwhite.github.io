b80dfb22026aee04cfa2d93013754cc0832e5a07
White <mwhite@webassign.net>
2017-03-19 09:41:51 -0400

    Trying a different method

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var bg = angular.module('bgFramework', [
-    'ui.router',
     'alienExplorers'
-]).config(
-    ($stateProvider, $locationProvider) => {
-        $locationProvider.html5Mode(true);
-        $stateProvider
-            .state('alienExplorers', {
-                url: '/AlienExplorers',
-                views: {
-                   '': {
-                         templateUrl: 'AlienExplorers.html',
-                         controller: 'alienExplorersCtrl'
-                       }
-                   }
-			   }
-            );
+]);
+
+bg.component('alienExplorers', {
+    controller: 'AEController as $ctrl',
+    parent: 'bgFramework',
+    templateUrl: 'AlienExplorers/AlienExplorers.html',
+    url: '/AlienExplorers'
 });
+
+// .config(
+//     ($stateProvider, $locationProvider) => {
+//         $locationProvider.html5Mode(true);
+//         $stateProvider
+//             .state('alienExplorers', {
+//                 url: '/AlienExplorers',
+//                 views: {
+//                    '': {
+//                          templateUrl: 'AlienExplorersgi.html',
+//                          controller: 'alienExplorersCtrl'
+//                        }
+//                    }
+// 			   }
+//             );
+// });
