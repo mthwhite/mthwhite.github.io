4bda326ec35d956db5b5a25480c8f4058dca7f72
White <mwhite@webassign.net>
2017-04-07 20:41:04 -0400

    iHi

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-angular.module('bgFramework', ['Injector.Service']);
+angular.module('bgFramework', [/*'Injector.Service'*/]);
 
-(function () {
-    angular
-        .module('bgFramework')
-        .config(ConfigureModule);
-
-    ConfigureModule.$inject = ['$stateProvider'];
-
-    function ConfigureModule($stateProvider) {
-        $stateProvider
-            .state('alienExplorers', {
-                controller: 'alienExplorersCtrl as $ctrl',
-                parent: 'bgFramework',
-                templateUrl: '/AlienExplorers/alienExplorers.html',
-                title: 'Alien Explorers',
-                url: '/alienExplorers'
-            })
-            .state('pokemon', {
-                controller: 'pokemonCtrl as $ctrl',
-                parent: 'bgFramework',
-                templateUrl: '/Pokemon/pokemon.html',
-                title: 'Pokemon',
-                url: '/pokemon'
-            });
-    }
-}());
+// (function () {
+//     angular
+//         .module('bgFramework')
+//         .config(ConfigureModule);
+//
+//     ConfigureModule.$inject = ['$stateProvider'];
+//
+//     function ConfigureModule($stateProvider) {
+//         $stateProvider
+//             .state('alienExplorers', {
+//                 controller: 'alienExplorersCtrl as $ctrl',
+//                 parent: 'bgFramework',
+//                 templateUrl: '/AlienExplorers/alienExplorers.html',
+//                 title: 'Alien Explorers',
+//                 url: '/alienExplorers'
+//             })
+//             .state('pokemon', {
+//                 controller: 'pokemonCtrl as $ctrl',
+//                 parent: 'bgFramework',
+//                 templateUrl: '/Pokemon/pokemon.html',
+//                 title: 'Pokemon',
+//                 url: '/pokemon'
+//             });
+//     }
+// }());
 
 (function () {
     angular
