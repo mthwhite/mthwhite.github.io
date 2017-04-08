e529c4d8b8f495a6cf3a32ec97fe8b7d2395e64a
White <mwhite@webassign.net>
2017-04-07 20:10:58 -0400

    Attempting to restart development

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-var bg = angular.module('bgFramework', [
-    'alienExplorers'
-]);
+angular.module('bgFramework', ['Injector.Service']);
 
+(function () {
+    angular
+        .module('bgFramework')
+        .config(ConfigureModule);
 
-// .config(
-//     ($stateProvider, $locationProvider) => {
-//         $locationProvider.html5Mode(true);
-//         $stateProvider
-//             .state('alienExplorers', {
-//                 url: '/AlienExplorers',
-//                 views: {
-//                    '': {
-//                          templateUrl: 'AlienExplorersgi.html',
-//                          controller: 'alienExplorersCtrl'
-//                        }
-//                    }
-// 			   }
-//             );
-// });
+    ConfigureModule.$inject = ['$stateProvider'];
+
+    function ConfigureModule($stateProvider) {
+        $stateProvider
+            .state('app.questionManagement.index', {
+                controller: 'alienExplorersCtrl as $ctrl',
+                parent: 'bgFramework',
+                templateUrl: '/alienExplorers/alienExplorers.html',
+                title: 'Alien Explorers'
+                url: '/alienExplorers'
+            })
+            .state('pokemon', {
+                controller: 'pokemonCtrl as $ctrl',
+                parent: 'bgFramework',
+                templateUrl: '/pokemon/pokemon.html',
+                title: 'Pokemon',
+                url: '/pokemon'
+            });
+    }
+}());
